54a8772dd6bfc37a70bb8f48c1f9b50652a4acd1
Maksym Shaptala <maxshaptala@gmail.com>
2023-07-04 20:50:44 +0300

    Added main controller

---
+++ src/main/java/org/itstep/diplom/controller/homeController.java
@@ -0,0 +1,10 @@
+package org.itstep.diplom.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class homeController {
+    @GetMapping("/")
+    public String index(){
+        return "index" ;
+    }
+}
